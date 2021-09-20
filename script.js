ae72989392b30d5fb5ee53b5ec1bf41b23be46ec
Margaret <mrobinson526@gmail.com>
2021-09-20 13:37:30 -0500

    with Canvas

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 window.addEventListener("DOMContentLoaded", function() {
-    let count=1;
     let btn = document.createElement("button");
     btn.innerHTML="Add Square";
     document.body.appendChild(btn);
-    });
-    
-    var btn=document.querySelector("button");
-    var container=document.querySelector("#count");
+});
+var canvas=document.createElement("canvas")
+    canvas.style.width="100%";
+    canvas.style.height="100%";
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight;
+    canvas.style.position="absolute";
+    canvas.style.left=10;
+    canvas.style.top=10;
+    canvas.style.zindex=100000;
+    canvas.style.pointerEvents="none";
+    document.body.appendChild(canvas);
+    var context=canvas.getContext("2d");
+    let counter=1;
     btn.addEventListener("click", function() {
-        var square=document.createElement("div")
-            square.className="box"
-            square.style.width="20px";
-            square.style.height="20px";
-            square.style.backgroundColor="black";
-        container.appendChild(square)
-    })
+        context.rect(20,20,50,60);
+        context.fillstyle="black";
+        context.fill(); 
+        document.body.appendChild()           
+    
+btn=document.getElementById("btn");
+btn.addEventListener("click", function() {
+let container=document.createElement("div");
+container.className="div";
+document.body.appendChild(container);
+ let btn=document.querySelector("button");
+let container=document.querySelector("div");
+
+
+});
+
+
+
+
+
